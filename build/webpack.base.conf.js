c8d5068e583755c94c0615db9162b9e3f7e4d4e9
jinglf000@gmail.com <jinglf000@gmail.com>
2017-04-04 23:15:08 +0800

    vue-cli 项目

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,86 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+// @ 被当作为src目录
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+	  '$': resolve('node_modules'),
+	  'bootstrap': path.resolve(__dirname,'../node_modules/bootstrap/dist/css/bootstrap.min.css'),
+	  'jquery' : 'jquery'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test : /\.css$/,
+        use  : ExtractTextPlugin.extract({
+			use : 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins : [
+	  new webpack.ProvidePlugin({
+		  $ : "jquery",
+		  jQuery : "jquery"
+	  })
+  ]
+}
